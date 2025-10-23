c49f30a36d4f85a5ecbb3ebb15e7f2d72b11367b
Musharaf Altaf <musharafdar10@Gmail.com>
2025-10-23 23:28:06 +0530

    little bit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -19,7 +19,7 @@ const generateAccessTokenandRefreshToken = async(userId)=>{
     }
 }
 
-
+            // what i do
     // GET USER DETAILS FROM FRONTENT
     // VALIDATION - NOT EMPTY
     // CHECK IF USER ALREADY EXISTS  USERNAME,EMAIL
